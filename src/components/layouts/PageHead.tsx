6e32df6cdf8b55ebfcac03f575c47ede097647cd
Taeuk Gwak <gwak2837@kakao.com>
2021-06-03 17:45:04 +0900

    Fix text

--- src/components/layouts/PageHead.tsx
+++ src/components/layouts/PageHead.tsx
@@ -11,8 +11,8 @@ type Props = {
 
 function PageHead({
   children,
-  title = '디저트핏 (Dessert Fit)',
-  description = '디저트핏은 내가 원하는 디저트를 쉽고 빠르게 고를 수 있는 온라인 디저트 검색 및 배달 서비스입니다.',
+  title = '이메일 블록체인 (Email Blockchain)',
+  description = '이메일 내용과 첨부 파일을 블록체인에 안전하게 보관하는 서비스입니다.',
 }: Props) {
   const { pathname } = useRouter()
 
@@ -23,9 +23,9 @@ function PageHead({
         <meta name="description" content={description} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content="/sindy.jpeg" />
+        <meta property="og:image" content="/favicon.ico" />
         <meta property="og:url" content={`${canonicalUrl}${pathname.slice(1)}`} />
-        <meta property="og:site_name" content="디저트핏 (Dessert Fit)" />
+        <meta property="og:site_name" content="이메일 블록체인" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image:alt" content="Sindy Logo" />
