b08fa5a5d7f590cba4b85aa035b60340faa207f1
Taeuk Gwak <gwak2837@kakao.com>
2021-06-16 23:08:31 +0900

    Add navigation bar

--- src/components/layouts/NavigationLayout.tsx
+++ src/components/layouts/NavigationLayout.tsx
@@ -1,26 +1,29 @@
-import { memo } from 'react'
-import { HEADER_HEIGHT, TABLET_MIN_WIDTH } from 'src/models/constants'
+import { ReactNode, memo } from 'react'
+import styled from 'styled-components'
 import ClientSideLink from 'src/components/atoms/ClientSideLink'
 import { FlexContainerAlignCenter } from 'src/styles/FlexContainer'
-import styled from 'styled-components'
 
 const FixedHeader = styled.header`
-  position: fixed;
-  bottom: 0;
-  z-index: 1;
-  width: 100%;
-  max-width: ${TABLET_MIN_WIDTH};
-  height: ${HEADER_HEIGHT};
+  width: fit-content;
+  height: 100vh;
+  display: inline-block;
+
   box-shadow: 0 -2px 5px 0 rgba(142, 142, 142, 0.25);
-  background-color: #ffffff;
+  background-color: #3c505f;
+
+  h5 {
+    color: #eee;
+  }
 `
 
 const FlexContainerAroundNav = styled.nav`
   display: flex;
+  flex-flow: column nowrap;
   justify-content: space-around;
   align-items: center;
 
   height: 100%;
+  padding: 1rem;
 
   > a {
     flex-grow: 1;
@@ -39,35 +42,34 @@ const NoMarginH6 = styled.h5`
   margin: 0;
 `
 
-const username = 'username'
-
 function Header() {
   return (
     <FixedHeader>
       <FlexContainerAroundNav>
-        <ClientSideLink href="/">
+        <ClientSideLink href="/documents/sending">
           <FlexContainerColumnCenterCenter>
-            <NoMarginH6>홈</NoMarginH6>
+            <NoMarginH6>FTP 문서 발신</NoMarginH6>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
-        <ClientSideLink href="/feed">
+        <ClientSideLink href="/documents">
           <FlexContainerColumnCenterCenter>
-            <NoMarginH6>소식</NoMarginH6>
+            <NoMarginH6>문서 수발신 목록</NoMarginH6>
+            <NoMarginH6>(문서관리대장)</NoMarginH6>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
-        <ClientSideLink href={`/users/${username}/favorite-menus`}>
+        <ClientSideLink href="/documents/history">
           <FlexContainerColumnCenterCenter>
-            <NoMarginH6>찜</NoMarginH6>
+            <NoMarginH6>문서 변경이력 조회</NoMarginH6>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
-        <ClientSideLink href={`/users/${username}/orders`}>
+        <ClientSideLink href="/documents/relating">
           <FlexContainerColumnCenterCenter>
-            <NoMarginH6>주문내역</NoMarginH6>
+            <NoMarginH6>연관문서 조회</NoMarginH6>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
-        <ClientSideLink href={`/users/${username}`}>
+        <ClientSideLink href="/documents/prove">
           <FlexContainerColumnCenterCenter>
-            <NoMarginH6>MY</NoMarginH6>
+            <NoMarginH6>진위검증</NoMarginH6>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
       </FlexContainerAroundNav>
@@ -75,4 +77,19 @@ function Header() {
   )
 }
 
-export default memo(Header)
+const MemoizedHeader = memo(Header)
+
+type Props = {
+  children: ReactNode
+}
+
+function NavigationLayout({ children }: Props) {
+  return (
+    <>
+      <MemoizedHeader />
+      {children}
+    </>
+  )
+}
+
+export default NavigationLayout
