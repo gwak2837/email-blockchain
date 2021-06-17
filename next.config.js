a4a54f99320cccd7ddaca9d59479f987c29ee39c
Taeuk Gwak <gwak2837@kakao.com>
2021-06-17 12:17:43 +0900

    Remove pwa

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withPWA = require('next-pwa')
-
-module.exports = withPWA({
+module.exports = {
   i18n: {
     locales: ['ko', 'en'],
     defaultLocale: 'ko',
   },
   poweredByHeader: process.env.NODE_ENV === 'development',
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
   reactStrictMode: process.env.NODE_ENV === 'development',
-})
+}
