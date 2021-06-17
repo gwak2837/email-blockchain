7a4194ba79018c5709601c4af6c8d54f650bdba5
Taeuk Gwak <gwak2837@kakao.com>
2021-06-17 12:18:05 +0900

    Add top header

--- src/components/layouts/NavigationLayout.tsx
+++ src/components/layouts/NavigationLayout.tsx
@@ -2,18 +2,26 @@ import { ReactNode, memo } from 'react'
 import styled from 'styled-components'
 import ClientSideLink from 'src/components/atoms/ClientSideLink'
 import { FlexContainerAlignCenter } from 'src/styles/FlexContainer'
+import { useRouter } from 'next/router'
 
-const FixedHeader = styled.header`
-  width: fit-content;
+const NAVIGATION_WIDTH = '11rem'
+
+const FixedPosition = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: ${NAVIGATION_WIDTH};
   height: 100vh;
   display: inline-block;
 
   box-shadow: 0 -2px 5px 0 rgba(142, 142, 142, 0.25);
-  background-color: #3c505f;
+  background-color: #324557;
+`
 
-  h5 {
-    color: #eee;
-  }
+const NavigationPadding = styled.div`
+  padding-left: ${NAVIGATION_WIDTH};
+  height: 100vh;
 `
 
 const FlexContainerAroundNav = styled.nav`
@@ -23,61 +31,100 @@ const FlexContainerAroundNav = styled.nav`
   align-items: center;
 
   height: 100%;
-  padding: 1rem;
 
   > a {
     flex-grow: 1;
+    width: 100%;
     height: 100%;
   }
 `
 
-const FlexContainerColumnCenterCenter = styled(FlexContainerAlignCenter)`
+const HorizontalBorder = styled.div`
+  width: 100%;
+  border: 1px solid #eee;
+`
+
+const FlexContainerColumnCenterCenter = styled(FlexContainerAlignCenter)<{ selected: boolean }>`
   flex-flow: column nowrap;
   justify-content: center;
 
   height: 100%;
+  padding: 1rem;
+  ${(p) => (p.selected ? 'background: #313545' : '')}
 `
 
-const NoMarginH6 = styled.h5`
-  margin: 0;
+const PaddingCenterH4 = styled.h4`
+  text-align: center;
+  padding: 0 0.5rem;
+  color: #eee;
 `
 
-function Header() {
+function Navigation() {
+  const { asPath } = useRouter()
+
   return (
-    <FixedHeader>
+    <FixedPosition>
       <FlexContainerAroundNav>
+        <ClientSideLink href="/">
+          <FlexContainerColumnCenterCenter selected={asPath === '/'}>
+            <PaddingCenterH4>블록체인 기반</PaddingCenterH4>
+            <PaddingCenterH4>이메일 문서 관리 시스템</PaddingCenterH4>
+          </FlexContainerColumnCenterCenter>
+        </ClientSideLink>
+        <HorizontalBorder />
         <ClientSideLink href="/documents/sending">
-          <FlexContainerColumnCenterCenter>
-            <NoMarginH6>FTP 문서 발신</NoMarginH6>
+          <FlexContainerColumnCenterCenter selected={asPath === '/documents/sending'}>
+            <PaddingCenterH4>FTP 문서 발신</PaddingCenterH4>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
         <ClientSideLink href="/documents">
-          <FlexContainerColumnCenterCenter>
-            <NoMarginH6>문서 수발신 목록</NoMarginH6>
-            <NoMarginH6>(문서관리대장)</NoMarginH6>
+          <FlexContainerColumnCenterCenter selected={asPath === '/documents'}>
+            <PaddingCenterH4>문서 수발신 목록</PaddingCenterH4>
+            <PaddingCenterH4>(문서관리대장)</PaddingCenterH4>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
         <ClientSideLink href="/documents/history">
-          <FlexContainerColumnCenterCenter>
-            <NoMarginH6>문서 변경이력 조회</NoMarginH6>
+          <FlexContainerColumnCenterCenter selected={asPath === '/documents/history'}>
+            <PaddingCenterH4>문서 변경이력 조회</PaddingCenterH4>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
         <ClientSideLink href="/documents/relating">
-          <FlexContainerColumnCenterCenter>
-            <NoMarginH6>연관문서 조회</NoMarginH6>
+          <FlexContainerColumnCenterCenter selected={asPath === '/documents/relating'}>
+            <PaddingCenterH4>연관문서 조회</PaddingCenterH4>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
         <ClientSideLink href="/documents/prove">
-          <FlexContainerColumnCenterCenter>
-            <NoMarginH6>진위검증</NoMarginH6>
+          <FlexContainerColumnCenterCenter selected={asPath === '/documents/prove'}>
+            <PaddingCenterH4>진위검증</PaddingCenterH4>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
       </FlexContainerAroundNav>
-    </FixedHeader>
+    </FixedPosition>
   )
 }
 
-const MemoizedHeader = memo(Header)
+const MemoizedNavigation = memo(Navigation)
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+`
+
+const GridContainerRow = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr;
+`
+
+const StyledHeader = styled.header`
+  width: 100%;
+  background: #21579a;
+  color: #eee;
+`
+
+const RightAlign = styled.div`
+  text-align: right;
+  padding: 1rem;
+`
 
 type Props = {
   children: ReactNode
@@ -85,10 +132,16 @@ type Props = {
 
 function NavigationLayout({ children }: Props) {
   return (
-    <>
-      <MemoizedHeader />
-      {children}
-    </>
+    <GridContainer>
+      <MemoizedNavigation />
+      <NavigationPadding />
+      <GridContainerRow>
+        <StyledHeader>
+          <RightAlign>홍길동</RightAlign>
+        </StyledHeader>
+        <main>{children}</main>
+      </GridContainerRow>
+    </GridContainer>
   )
 }
 
