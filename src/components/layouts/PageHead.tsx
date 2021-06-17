a074861c1a8dae633241be43c953adddf13404de
Taeuk Gwak <gwak2837@kakao.com>
2021-06-17 14:16:25 +0900

    Modify images

--- src/components/layouts/PageHead.tsx
+++ src/components/layouts/PageHead.tsx
@@ -23,12 +23,12 @@ function PageHead({
         <meta name="description" content={description} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content="/favicon.ico" />
+        <meta property="og:image" content="/og-image.webp" />
         <meta property="og:url" content={`${canonicalUrl}${pathname}`} />
         <meta property="og:site_name" content="이메일 블록체인" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image:alt" content="Sindy Logo" />
+        <meta name="twitter:image:alt" content="Email Blockchain" />
       </Head>
       {children}
     </>
