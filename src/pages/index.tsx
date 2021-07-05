610fac7b619fa2de9a8fce9ca99422cec1fd5037
Taeuk Gwak <gwak2837@kakao.com>
2021-07-05 14:12:02 +0900

    Add login form on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,183 @@
+import { LockTwoTone, UnlockTwoTone } from '@ant-design/icons'
+import { Button, Checkbox, Divider, Input } from 'antd'
+import { useState, useCallback } from 'react'
+import { useForm, SubmitHandler, Controller } from 'react-hook-form'
+import ClientSideLink from 'src/components/atoms/ClientSideLink'
+import { sleep } from 'src/utils/commons'
+import styled from 'styled-components'
 import NavigationLayout from '../components/layouts/NavigationLayout'
 import PageHead from '../components/layouts/PageHead'
 
+const FlexContainerCenterCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+`
+
+const CenterH1 = styled.h1`
+  text-align: center;
+`
+
+const GridContainerForm = styled.form`
+  display: grid;
+  grid-template-columns: minmax(auto, 370px);
+  justify-content: center;
+
+  padding: 1rem;
+`
+
+const ContinueLoginDiv = styled.div`
+  margin: 0 0 1rem;
+  text-align: right;
+`
+
+const FlexContainerAroundCenter = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const RedText = styled.h5`
+  margin: 0.5rem 0.2rem;
+  color: #800000;
+`
+
+const Padding = styled.div`
+  padding: 0.5rem;
+`
+
+const validateEmail = {
+  required: '필수 항목입니다.',
+  maxLength: {
+    value: 64,
+    message: '최대 64글자 이하로 입력해주세요.',
+  },
+  pattern: {
+    value: /\S+@\S+\.\S+/,
+    message: '이메일을 형식에 맞게 입력해주세요.',
+  },
+}
+
+const validatePassword = {
+  required: '필수 항목입니다.',
+  minLength: {
+    value: 8,
+    message: '최소 8글자 이상 입력해주세요.',
+  },
+}
+
+const PASSWORD_INPUT_ICONS = [
+  <UnlockTwoTone key={1} style={{ fontSize: '1.2rem' }} twoToneColor="#c4801a" />,
+  <LockTwoTone key={2} style={{ fontSize: '1.2rem' }} twoToneColor="#52c41a" />,
+]
+
+function renderPasswordInputIcon(visible: boolean) {
+  return visible ? PASSWORD_INPUT_ICONS[0] : PASSWORD_INPUT_ICONS[1]
+}
+
+type LoginFormValues = {
+  email: string
+  password: string
+  remember: boolean
+}
+
 function HomePage() {
+  const [loading, setIsLoading] = useState(false)
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<LoginFormValues>({
+    defaultValues: { email: '', password: '', remember: true },
+  })
+
+  const onSubmit = useCallback<SubmitHandler<LoginFormValues>>(async ({ email, password }) => {
+    setIsLoading(true)
+
+    console.log({ email, password })
+    await sleep(3000)
+
+    setIsLoading(false)
+  }, [])
+
   return (
     <PageHead>
-      <NavigationLayout>홈페이지</NavigationLayout>
+      <NavigationLayout>
+        <FlexContainerCenterCenter>
+          <div>
+            <CenterH1>로그인</CenterH1>
+            <GridContainerForm onSubmit={handleSubmit(onSubmit)}>
+              <label htmlFor="email">
+                <h4>이메일</h4>
+                <Controller
+                  control={control}
+                  name="email"
+                  render={({ field }) => (
+                    <Input
+                      disabled={loading}
+                      placeholder="example@email.com"
+                      size="large"
+                      type="email"
+                      {...field}
+                    />
+                  )}
+                  rules={validateEmail}
+                />
+                <RedText>{errors.email ? errors.email.message : <br />}</RedText>
+              </label>
+
+              <label htmlFor="password">
+                <h4>비밀번호</h4>
+                <Controller
+                  control={control}
+                  name="password"
+                  render={({ field }) => (
+                    <Input.Password
+                      disabled={loading}
+                      iconRender={renderPasswordInputIcon}
+                      placeholder="qwer1234!"
+                      size="large"
+                      type="password"
+                      {...field}
+                    />
+                  )}
+                  rules={validatePassword}
+                />
+                <RedText>{errors.password ? errors.password.message : <br />}</RedText>
+              </label>
+
+              <ContinueLoginDiv>
+                <Controller
+                  control={control}
+                  name="remember"
+                  render={({ field }) => (
+                    <Checkbox checked={field.value} disabled={loading} {...field}>
+                      로그인 유지
+                    </Checkbox>
+                  )}
+                />
+              </ContinueLoginDiv>
+
+              <Button loading={loading} htmlType="submit" size="large">
+                로그인
+              </Button>
+
+              <FlexContainerAroundCenter>
+                <ClientSideLink href="/register">
+                  <Padding>회원가입 페이지</Padding>
+                </ClientSideLink>
+                <Divider type="vertical" />
+                <ClientSideLink href="/find/password">
+                  <Padding>비밀번호 찾기</Padding>
+                </ClientSideLink>
+              </FlexContainerAroundCenter>
+            </GridContainerForm>
+          </div>
+        </FlexContainerCenterCenter>
+      </NavigationLayout>
     </PageHead>
   )
 }
