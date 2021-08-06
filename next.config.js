f2a0af194ed0ec3a21b80a5e2bb8d2417712b4d9
Taeuk Gwak <gwak2837@kakao.com>
2021-08-06 17:38:45 +0900

    Add next pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
   i18n: {
     locales: ['ko', 'en'],
     defaultLocale: 'ko',
   },
   poweredByHeader: process.env.NODE_ENV === 'development',
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
   reactStrictMode: process.env.NODE_ENV === 'development',
-}
+})
