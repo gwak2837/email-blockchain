82caec0734b0573e2302c4db1f32d06163caf3a5
Taeuk Gwak <gwak2837@kakao.com>
2021-08-06 16:46:15 +0900

    Update pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,17 +44,26 @@ export default class EmailBlockchainDocument extends Document {
           />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="icon" href="/favicon.ico" />
-          <link rel="apple-touch-startup-image" href="/splash.png" />
-          <link rel="apple-touch-icon" href="/favicon.ico" />
           <link rel="canonical" href={canonicalUrl} />
           <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/favicon.ico" />
           <meta name="author" content="C-Link" />
           <meta name="keywords" content={keywords} />
           <meta name="application-name" content="이메일 블록체인" />
-          <meta name="apple-mobile-web-app-title" content="이메일 블록체인" />
+          <meta name="theme-color" content="#26326d" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-title" content="이메일 블록체인" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="description" content="이메일 첨부파일을 블록체인으로 관리하는 곳" />
+          <meta property="og:title" content="이메일 블록체인" />
+          <meta property="og:description" content="이메일 첨부파일을 블록체인으로 관리하는 곳" />
+          <meta property="og:image" content="/favicon.ico" />
+          <meta property="og:url" content={`${canonicalUrl}`} />
+          <meta property="og:site_name" content="이메일 블록체인 (Email Blockchain)" />
+          <meta property="og:type" content="website" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image:alt" content="Email Blockchain Logo" />
         </Head>
         <body>
           <Main />
