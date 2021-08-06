bf5c2013b7dbd41e9dcf4439c0a74d9d56ca0290
Taeuk Gwak <gwak2837@kakao.com>
2021-08-06 16:37:24 +0900

    Add rich text editor

--- src/pages/send.tsx
+++ src/pages/send.tsx
@@ -1,53 +1,65 @@
 import { UploadOutlined } from '@ant-design/icons'
 import { Input, Upload, Button } from 'antd'
+import { useState } from 'react'
 import NavigationLayout from 'src/components/layouts/NavigationLayout'
 import PageHead from 'src/components/layouts/PageHead'
 import { GridContainerAlignCenter } from './documents/history'
+import { Editor } from '@tinymce/tinymce-react'
 
 const searchInputStyle = {
   width: '50%',
   minWidth: '20rem',
 }
 
+const init = {
+  height: 500,
+  menubar: false,
+  plugins: [
+    'advlist autolink lists link image',
+    'charmap print preview anchor help',
+    'searchreplace visualblocks code',
+    'insertdatetime media table paste wordcount',
+  ],
+  toolbar:
+    'undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | help',
+}
+
 const description = ''
 
 function SendingEmailPage() {
+  function handleEditorChange(e: any) {
+    console.log(e.target.getContent())
+  }
+
   return (
     <PageHead title="이메일 블록체인 - 이메일 보내기" description={description}>
       <NavigationLayout>
         <GridContainerAlignCenter>
           <h3>받는 사람</h3>
-          <Input
-            placeholder="블록체인에 저장된 이벤트 선택"
-            size="large"
-            style={searchInputStyle}
-          />
+          <Input placeholder="받는사람" size="large" style={searchInputStyle} />
         </GridContainerAlignCenter>
         <GridContainerAlignCenter>
           <h3>참조</h3>
-          <Input
-            placeholder="블록체인에 저장된 이벤트 선택"
-            size="large"
-            style={searchInputStyle}
-          />
+          <Input placeholder="참조" size="large" style={searchInputStyle} />
         </GridContainerAlignCenter>
         <GridContainerAlignCenter>
           <h3>제목</h3>
-          <Input
-            placeholder="블록체인에 저장된 이벤트 선택"
-            size="large"
-            style={searchInputStyle}
-          />
+          <Input placeholder="제목" size="large" style={searchInputStyle} />
         </GridContainerAlignCenter>
         <GridContainerAlignCenter>
           <h3>파일 첨부</h3>
-          <Input placeholder="파일 업로드" size="large" style={searchInputStyle} />
           <Upload>
             <Button icon={<UploadOutlined />} size="large">
               Upload
             </Button>
           </Upload>
         </GridContainerAlignCenter>
+        <Editor
+          apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY}
+          initialValue="<p>Initial content</p>"
+          init={init}
+          onChange={handleEditorChange}
+        />
       </NavigationLayout>
     </PageHead>
   )
